c94efb34769c3ece37e70b477be2d3e93f0357a1
Arthur <arthur.gomes_4718@hotmail.com>
2019-11-30 18:39:50 -0200

    Finished Project.
    Added New word Activity.
    Added view model reference inside the activity.
    Added Room database callback.

--- app/src/main/java/com/devarthur/roomwordsample/data/WordRoomDatabase.kt
+++ app/src/main/java/com/devarthur/roomwordsample/data/WordRoomDatabase.kt
@@ -4,7 +4,10 @@ import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
+import androidx.sqlite.db.SupportSQLiteDatabase
 import com.devarthur.roomwordsample.data.entity.Word
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.launch
 
 //* Created by Arthur Gomes at 2019-11-30 16:39 - contact me at devarthur4718@gmail.com.br
 // Annotates class to be a Room Database with a table (entity) of the Word class
@@ -13,13 +16,40 @@ public abstract class WordRoomDatabase : RoomDatabase() {
 
     abstract fun wordDao(): WordDao
 
+    private class WordDatabaseCallback(
+        private val scope: CoroutineScope
+    ) : RoomDatabase.Callback() {
+
+        override fun onOpen(db: SupportSQLiteDatabase) {
+            super.onOpen(db)
+            INSTANCE?.let { database ->
+                scope.launch {
+                    var wordDao = database.wordDao()
+
+                    // Delete all content here.
+                    wordDao.deleteAll()
+
+                    // Add sample words.
+                    var word = Word("Hello")
+                    wordDao.insert(word)
+                    word = Word("World!")
+                    wordDao.insert(word)
+
+                    // TODO: Add your own words!
+                    word = Word("TODO!")
+                    wordDao.insert(word)
+                }
+            }
+        }
+    }
+
     companion object {
         // Singleton prevents multiple instances of database opening at the
         // same time.
         @Volatile
         private var INSTANCE: WordRoomDatabase? = null
 
-        fun getDatabase(context: Context): WordRoomDatabase {
+        fun getDatabase(context: Context, scope : CoroutineScope): WordRoomDatabase {
             val tempInstance = INSTANCE
             if (tempInstance != null) {
                 return tempInstance
@@ -29,7 +59,8 @@ public abstract class WordRoomDatabase : RoomDatabase() {
                     context.applicationContext,
                     WordRoomDatabase::class.java,
                     "word_database"
-                ).build()
+                ).addCallback(WordDatabaseCallback(scope))
+                    .build()
                 INSTANCE = instance
                 return instance
             }
