928e9cd694db4f5549586588e464a95a0042582d
Arthur <arthur.gomes_4718@hotmail.com>
2019-11-30 18:08:39 -0200

    Added Recycler View adapter.

--- app/src/main/java/com/devarthur/roomwordsample/ui/MainActivity.kt
+++ app/src/main/java/com/devarthur/roomwordsample/ui/MainActivity.kt
@@ -2,12 +2,23 @@ package com.devarthur.roomwordsample.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.devarthur.roomwordsample.R
+import com.devarthur.roomwordsample.adapter.WordListAdapter
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        initActions()
+    }
+
+    private fun initActions() { val recyclerView = findViewById<RecyclerView>(R.id.recyclerview)
+        val adapter = WordListAdapter(this)
+        recyclerView.adapter = adapter
+        recyclerView.layoutManager = LinearLayoutManager(this)
+
     }
 }
