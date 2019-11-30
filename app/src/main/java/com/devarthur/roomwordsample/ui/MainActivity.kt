55f0949e30aa12728fccac14c80fc7ea8c99df39
Arthur <arthur.gomes_4718@hotmail.com>
2019-11-30 18:52:43 -0200

    Added codelab link.

--- app/src/main/java/com/devarthur/roomwordsample/ui/MainActivity.kt
+++ app/src/main/java/com/devarthur/roomwordsample/ui/MainActivity.kt
@@ -14,6 +14,7 @@ import com.devarthur.roomwordsample.adapter.WordListAdapter
 import com.devarthur.roomwordsample.data.entity.Word
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 
+//Sample created from https://codelabs.developers.google.com/codelabs/android-room-with-a-view-kotlin/#14
 class MainActivity : AppCompatActivity() {
 
     private lateinit var wordViewModel: WordViewModel
